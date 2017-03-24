f543b8f4e94a86ff7fc3d865d912a45d5ac0c978
Charles <ccbpeden@warpmail.net>
2017-03-24 09:24:51 -0700

    define data models, correct seed.json

--- app/models/comment.js
+++ app/models/comment.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  user: DS.attr(),
+  commentBody: DS.attr(),
+  voteStatus: DS.attr(),
+  post: DS.belongsTO('post', {async: true})
 });
--- app/models/post.js
+++ app/models/post.js
@@ -1,5 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  user: DS.attr(),
+  title: DS.attr(),
+  teaser: DS.attr(),
+  postBody: DS.attr(),
+  tags: DS.attr(),
+  comments: DS.hasMany('comment', {async: true})
 });
